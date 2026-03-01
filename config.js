3a74ac800990461c284862e71df5b647f1567079
simonvelucia-afk <simon.velucia@gmail.com>
2026-03-01 15:29:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  Tableau de bord Supabase → Settings → API
 // ═══════════════════════════════════════════════════════════════
 
-const SUPABASE_URL     = "sb_publishable_hLBeLa1MjRwNH_WrCSl9kw__eREOwXk";
+const SUPABASE_URL     = "https://twgtthuvcwccuzuicynh.supabase.co";
 // ex: "https://abcdefghijkl.supabase.co"
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3Z3R0aHV2Y3djY3V6dWljeW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTIzOTEsImV4cCI6MjA4Nzk2ODM5MX0.RhCm1FrK-3YA158IuAZA5caFjBSdhlPa_uwmGFCivYo";
