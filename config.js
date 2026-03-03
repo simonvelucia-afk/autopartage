5c211dc23115f6038271a49a0ae00f4d9447db40
simonvelucia-afk <simon.velucia@gmail.com>
2026-03-02 19:55:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 const SUPABASE_URL     = "https://twgtthuvcwccuzuicynh.supabase.co";
-// ex: "https://abcdefghijkl.supabase.co"
+// ex: "https://abcdefghijkl.supabase.co" 
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3Z3R0aHV2Y3djY3V6dWljeW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTIzOTEsImV4cCI6MjA4Nzk2ODM5MX0.RhCm1FrK-3YA158IuAZA5caFjBSdhlPa_uwmGFCivYo";
 // ex: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
